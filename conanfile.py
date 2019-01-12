476080a172731b9cd25e3c81342fd800fbad901e
Huang-Ming Huang <huangh@objectcomputing.com>
2019-01-12 13:50:53 -0600

    use patch for modifying cmake files

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ import os, shutil, os.path, glob
 
 class MongocdriverConan(ConanFile):
     name = "mongo-c-driver"
-    version = "1.10.0"
+    version = "1.10.3"
     description = "A high-performance MongoDB driver for C"
     topics = ("conan", "libmongoc", "mongodb")
     url = "http://github.com/bincrafters/conan-mongo-c-driver"
@@ -17,6 +17,7 @@ class MongocdriverConan(ConanFile):
     default_options = "shared=False"
         
     requires = 'zlib/1.2.11@conan/stable'
+    exports_sources = ["package.patch"]
     generators = "cmake"
 
     def configure(self):
@@ -32,19 +33,7 @@ class MongocdriverConan(ConanFile):
                           .format(self.version))
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, 'sources')
-        tools.replace_in_file("sources/CMakeLists.txt", "project (mongo-c-driver C)",
-                                      '''project (mongo-c-driver C)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
-        tools.replace_in_file("sources/src/libmongoc/CMakeLists.txt", 'SSL_LIBRARIES ${OPENSSL_LIBRARIES}', 'SSL_LIBRARIES ${OPENSSL_LIBRARIES} dl')
-        tools.replace_in_file("sources/src/libmongoc/CMakeLists.txt", "add_library (mongoc_shared", '''
-if (APPLE)
-  set(CMAKE_MACOS_RPATH 1)
-  set(CMAKE_INSTALL_RPATH "@executable_path")
-else()
-  set(CMAKE_INSTALL_RPATH "$ORIGIN")
-endif()
-add_library (mongoc_shared''')
+        tools.patch(base_path='sources', patch_file="package.patch")
 
     def _configure_cmake(self):
         cmake = CMake(self)
@@ -54,6 +43,9 @@ add_library (mongoc_shared''')
         cmake.definitions["ENABLE_BSON"] = "ON"
         cmake.definitions["ENABLE_SASL"] = "OFF"
         cmake.definitions["ENABLE_STATIC"] = "OFF" if self.options.shared else "ON"
+        
+        if tools.os_info.is_macos:
+          cmake.definitions["CMAKE_MACOSX_RPATH"] = "ON"
 
         cmake.configure(source_dir="sources")
 
@@ -65,23 +57,10 @@ add_library (mongoc_shared''')
 
     def package(self):
         self.copy(pattern="COPYING*", dst="licenses", src="sources")
-        self.copy(pattern="lib/cmake/lib*/*", dst=".", src="sources")
         
         # cmake installs all the files
         cmake = self._configure_cmake()
         cmake.install()
-        
-        if not self.options.shared:
-          # Remove the shared library from a static build, we don't keep them simply because their dependencies are statically linked, 
-          # which are propably not what we wanted.
-          builddir = os.getcwd()
-          package_id = os.path.basename(builddir)
-          package_path = os.path.join('../../package', package_id)
-          
-          shutil.rmtree(os.path.join(package_path, 'lib/cmake/libmongoc-1.0'))
-          shutil.rmtree(os.path.join(package_path,'lib/cmake/libbson-1.0'))
-          [os.remove(x) for x in glob.glob(os.path.join(package_path,"lib/libmongoc-1.0.*"))]
-          [os.remove(x) for x in glob.glob(os.path.join(package_path,"lib/libbson-1.0.*"))]
 
     def package_info(self):
         lib_suffix = "" if self.options["shared"] else "-static"
@@ -89,7 +68,6 @@ add_library (mongoc_shared''')
         libnames = ['mongoc', 'bson']
         self.cpp_info.libs = [ "{}{}-1.0".format(name, lib_suffix) for name in libnames ]
         self.cpp_info.includedirs.extend( ['include/lib{}'.format(name) for name in self.cpp_info.libs ] )
-        # self.cpp_info.builddirs = [ "lib/cmake/lib{}".format(name) for name in self.cpp_info.libs ]
         
         if tools.os_info.is_macos:
             self.cpp_info.exelinkflags = ['-framework CoreFoundation', '-framework Security']
